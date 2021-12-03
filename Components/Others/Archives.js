0a184e5a8b51d059c5124e4b1fc738e85262e582
Derkizzle <95419636+Derkizzle@users.noreply.github.com>
2021-12-03 11:16:43 +0000

    Update Archives.js
    Appended Community Lore to the end of Lore

--- Components/Others/Archives.js
+++ Components/Others/Archives.js
@@ -167,6 +167,232 @@ const Archives = () => {
                 </div>
               </div>
             </div>
+
+ <div>
+            <div className="div-block-11 flex">
+              <h1 className="heading-32">
+                <strong className="bold-text-63">The Jester of Yama</strong>
+                <br />- Derkizzle
+                <a href="#" className="div-block-40 w-inline-block d-inline">
+                  <svg
+                    className="navbar-icons"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 30 30"
+                    width="30px"
+                    height="30px"
+                  >
+                    {" "}
+                    <path d="M28,6.937c-0.957,0.425-1.985,0.711-3.064,0.84c1.102-0.66,1.947-1.705,2.345-2.951c-1.03,0.611-2.172,1.055-3.388,1.295 c-0.973-1.037-2.359-1.685-3.893-1.685c-2.946,0-5.334,2.389-5.334,5.334c0,0.418,0.048,0.826,0.138,1.215 c-4.433-0.222-8.363-2.346-10.995-5.574C3.351,6.199,3.088,7.115,3.088,8.094c0,1.85,0.941,3.483,2.372,4.439 c-0.874-0.028-1.697-0.268-2.416-0.667c0,0.023,0,0.044,0,0.067c0,2.585,1.838,4.741,4.279,5.23 c-0.447,0.122-0.919,0.187-1.406,0.187c-0.343,0-0.678-0.034-1.003-0.095c0.679,2.119,2.649,3.662,4.983,3.705 c-1.825,1.431-4.125,2.284-6.625,2.284c-0.43,0-0.855-0.025-1.273-0.075c2.361,1.513,5.164,2.396,8.177,2.396 c9.812,0,15.176-8.128,15.176-15.177c0-0.231-0.005-0.461-0.015-0.69C26.38,8.945,27.285,8.006,28,6.937z" />
+                  </svg>
+                </a>
+                <a
+                  href="#"
+                  className="div-block-40 w-inline-block d-inline pl-0 ml-0"
+                  style={{ paddingLeft: "0" }}
+                >
+                  <svg
+                    className="navbar-icons"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 50 50"
+                    width="30px"
+                    height="30px"
+                  >
+                    <path d="M 41.625 10.769531 C 37.644531 7.566406 31.347656 7.023438 31.078125 7.003906 C 30.660156 6.96875 30.261719 7.203125 30.089844 7.589844 C 30.074219 7.613281 29.9375 7.929688 29.785156 8.421875 C 32.417969 8.867188 35.652344 9.761719 38.578125 11.578125 C 39.046875 11.867188 39.191406 12.484375 38.902344 12.953125 C 38.710938 13.261719 38.386719 13.429688 38.050781 13.429688 C 37.871094 13.429688 37.6875 13.378906 37.523438 13.277344 C 32.492188 10.15625 26.210938 10 25 10 C 23.789063 10 17.503906 10.15625 12.476563 13.277344 C 12.007813 13.570313 11.390625 13.425781 11.101563 12.957031 C 10.808594 12.484375 10.953125 11.871094 11.421875 11.578125 C 14.347656 9.765625 17.582031 8.867188 20.214844 8.425781 C 20.0625 7.929688 19.925781 7.617188 19.914063 7.589844 C 19.738281 7.203125 19.34375 6.960938 18.921875 7.003906 C 18.652344 7.023438 12.355469 7.566406 8.320313 10.8125 C 6.214844 12.761719 2 24.152344 2 34 C 2 34.175781 2.046875 34.34375 2.132813 34.496094 C 5.039063 39.605469 12.972656 40.941406 14.78125 41 C 14.789063 41 14.800781 41 14.8125 41 C 15.132813 41 15.433594 40.847656 15.621094 40.589844 L 17.449219 38.074219 C 12.515625 36.800781 9.996094 34.636719 9.851563 34.507813 C 9.4375 34.144531 9.398438 33.511719 9.765625 33.097656 C 10.128906 32.683594 10.761719 32.644531 11.175781 33.007813 C 11.234375 33.0625 15.875 37 25 37 C 34.140625 37 38.78125 33.046875 38.828125 33.007813 C 39.242188 32.648438 39.871094 32.683594 40.238281 33.101563 C 40.601563 33.515625 40.5625 34.144531 40.148438 34.507813 C 40.003906 34.636719 37.484375 36.800781 32.550781 38.074219 L 34.378906 40.589844 C 34.566406 40.847656 34.867188 41 35.1875 41 C 35.199219 41 35.210938 41 35.21875 41 C 37.027344 40.941406 44.960938 39.605469 47.867188 34.496094 C 47.953125 34.34375 48 34.175781 48 34 C 48 24.152344 43.785156 12.761719 41.625 10.769531 Z M 18.5 30 C 16.566406 30 15 28.210938 15 26 C 15 23.789063 16.566406 22 18.5 22 C 20.433594 22 22 23.789063 22 26 C 22 28.210938 20.433594 30 18.5 30 Z M 31.5 30 C 29.566406 30 28 28.210938 28 26 C 28 23.789063 29.566406 22 31.5 22 C 33.433594 22 35 23.789063 35 26 C 35 28.210938 33.433594 30 31.5 30 Z" />
+                  </svg>
+                </a>
+                <br />
+              </h1>
+            </div>
+            <div className="split flex-v2">
+              <div className="row mt-3">
+                <div className="col-md-6 col-sm-12 ">
+                  <p className="text-white-opacity-2 mb-40 max-width dilema font-16">
+                    A low red mist clears outside the window of the train
+                    carriage that I call home. Very much stationary train, I
+                    should add, the wheels stuck fast into rock that has melted
+                    and hardened from the heat of the volcano. I throw out the
+                    wilted purple flower that I picked last week from the rich
+                    volcanic soil on the road into Yama, and then start getting
+                    dressed for work. A jester must always be in costume, never
+                    revealing themselves as too human, too real.
+                    <br />
+                    <br />
+                    And me, more than most. I start with the thick bandaging
+                    that I wrap around my chest. One rotation, then another. My
+                    skin crinkles dark above the folds. And then the trousers
+                    cut low and loose around my crotch, the belt pulled in tight
+                    at my waist. I pull on the jacket, tucking it in tightly,
+                    setting the jagged edges of the collar neat against my
+                    shoulders. The red baubles of my hat knock against my face,
+                    bright balls that signal my profession. It was my twin
+                    brother who gave this costume to me, the day he finally
+                    found the courage to leave Yama, to go and join the coastal
+                    warriors of Engan. I stayed behind, pulled between my love
+                    for home and my cynical anger at the corruption of our
+                    leaders. I took over my brother’s role, his work, his life.
+                    I even took over his name. The warriors at the palace could
+                    not tell the difference. My voice a little higher, my body a
+                    little softer, but the differences are too small for those
+                    in power to bother to notice.
+                    <br />I set out on to the road, gripping my sword tighter
+                    than usual today. The road to the palace feels dangerous,
+                    uneasy, as though everyone is waiting for a sign. Perhaps it
+                    is the moon that sat heavy and red all night, or maybe it is
+                    the low rumbles of the mountain that has interrupted the
+                    sleep of the city for the last few days. Children play in
+                    the rusty dust of the road and women scream out the prices
+                    of the feeble produce that they attempt to sell. I ignore
+                    them all, even as the children run about my feet, begging me
+                    for a joke. I reach the palace, warriors milling in the
+                    quad. Energy fizzes with uncertainty, a new darkness that
+                    clogs the corners of the corridors, hooded men glancing
+                    anxiously at one another.
+                    <br />
+                    By the time I get to the hall, the councillors are ready to
+                    meet. I stand at the edge of the room, waiting. If I am
+                    lucky, they will forget I am even here. Nik the jester,
+                    harmless, good for a few cynical jibes and jests. Little do
+                    they know that I hear everything. I can sense their
+                    decisions, their bribes, their thoughtless words that led to
+                    lives lost, warriors sharpening their weapons. What would
+                    they really think of me if they knew how dangerous I could
+                    be?
+                    <br />
+                  </p>
+                </div>
+                <div className="col-md-6 col-sm-12 p-md-0">
+                  {" "}
+                  <img src="/assets/images/Derkizzle.jpeg" alt="Kiroh" />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div className="div-block-11 flex mt-5">
+              <h1 className="heading-32">
+                <strong className="bold-text-63">Haruto Kaiba</strong>
+                <a href="#" className="div-block-40 w-inline-block d-inline">
+                  <svg
+                    className="navbar-icons"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 30 30"
+                    width="30px"
+                    height="30px"
+                  >
+                    {" "}
+                    <path d="M28,6.937c-0.957,0.425-1.985,0.711-3.064,0.84c1.102-0.66,1.947-1.705,2.345-2.951c-1.03,0.611-2.172,1.055-3.388,1.295 c-0.973-1.037-2.359-1.685-3.893-1.685c-2.946,0-5.334,2.389-5.334,5.334c0,0.418,0.048,0.826,0.138,1.215 c-4.433-0.222-8.363-2.346-10.995-5.574C3.351,6.199,3.088,7.115,3.088,8.094c0,1.85,0.941,3.483,2.372,4.439 c-0.874-0.028-1.697-0.268-2.416-0.667c0,0.023,0,0.044,0,0.067c0,2.585,1.838,4.741,4.279,5.23 c-0.447,0.122-0.919,0.187-1.406,0.187c-0.343,0-0.678-0.034-1.003-0.095c0.679,2.119,2.649,3.662,4.983,3.705 c-1.825,1.431-4.125,2.284-6.625,2.284c-0.43,0-0.855-0.025-1.273-0.075c2.361,1.513,5.164,2.396,8.177,2.396 c9.812,0,15.176-8.128,15.176-15.177c0-0.231-0.005-0.461-0.015-0.69C26.38,8.945,27.285,8.006,28,6.937z" />
+                  </svg>
+                </a>
+                <a
+                  href="#"
+                  className="div-block-40 w-inline-block d-inline pl-0 ml-0"
+                  style={{ paddingLeft: "0" }}
+                >
+                  <svg
+                    className="navbar-icons"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 50 50"
+                    width="30px"
+                    height="30px"
+                  >
+                    <path d="M 41.625 10.769531 C 37.644531 7.566406 31.347656 7.023438 31.078125 7.003906 C 30.660156 6.96875 30.261719 7.203125 30.089844 7.589844 C 30.074219 7.613281 29.9375 7.929688 29.785156 8.421875 C 32.417969 8.867188 35.652344 9.761719 38.578125 11.578125 C 39.046875 11.867188 39.191406 12.484375 38.902344 12.953125 C 38.710938 13.261719 38.386719 13.429688 38.050781 13.429688 C 37.871094 13.429688 37.6875 13.378906 37.523438 13.277344 C 32.492188 10.15625 26.210938 10 25 10 C 23.789063 10 17.503906 10.15625 12.476563 13.277344 C 12.007813 13.570313 11.390625 13.425781 11.101563 12.957031 C 10.808594 12.484375 10.953125 11.871094 11.421875 11.578125 C 14.347656 9.765625 17.582031 8.867188 20.214844 8.425781 C 20.0625 7.929688 19.925781 7.617188 19.914063 7.589844 C 19.738281 7.203125 19.34375 6.960938 18.921875 7.003906 C 18.652344 7.023438 12.355469 7.566406 8.320313 10.8125 C 6.214844 12.761719 2 24.152344 2 34 C 2 34.175781 2.046875 34.34375 2.132813 34.496094 C 5.039063 39.605469 12.972656 40.941406 14.78125 41 C 14.789063 41 14.800781 41 14.8125 41 C 15.132813 41 15.433594 40.847656 15.621094 40.589844 L 17.449219 38.074219 C 12.515625 36.800781 9.996094 34.636719 9.851563 34.507813 C 9.4375 34.144531 9.398438 33.511719 9.765625 33.097656 C 10.128906 32.683594 10.761719 32.644531 11.175781 33.007813 C 11.234375 33.0625 15.875 37 25 37 C 34.140625 37 38.78125 33.046875 38.828125 33.007813 C 39.242188 32.648438 39.871094 32.683594 40.238281 33.101563 C 40.601563 33.515625 40.5625 34.144531 40.148438 34.507813 C 40.003906 34.636719 37.484375 36.800781 32.550781 38.074219 L 34.378906 40.589844 C 34.566406 40.847656 34.867188 41 35.1875 41 C 35.199219 41 35.210938 41 35.21875 41 C 37.027344 40.941406 44.960938 39.605469 47.867188 34.496094 C 47.953125 34.34375 48 34.175781 48 34 C 48 24.152344 43.785156 12.761719 41.625 10.769531 Z M 18.5 30 C 16.566406 30 15 28.210938 15 26 C 15 23.789063 16.566406 22 18.5 22 C 20.433594 22 22 23.789063 22 26 C 22 28.210938 20.433594 30 18.5 30 Z M 31.5 30 C 29.566406 30 28 28.210938 28 26 C 28 23.789063 29.566406 22 31.5 22 C 33.433594 22 35 23.789063 35 26 C 35 28.210938 33.433594 30 31.5 30 Z" />
+                  </svg>
+                </a>
+                <br />
+                <h5>Story narrated by an old villager ‘old man Akito ’</h5>
+              </h1>
+            </div>
+            <div className="split flex-v2">
+              <div className="row mt-3">
+                <div className="col-md-6 col-sm-12 ">
+                  <p className="text-white-opacity-2 mb-40 max-width dilema font-16">
+                    Many songs sing of this monster, many stories tell about
+                    this hero. Haruto the glutton, monster, Chi no okui,
+                    Kushinbo are a few of the names that have been used through
+                    the ages. But I knew the man, the man that saved this
+                    village. I can’t speak on the stories of him killing 300 men
+                    with his sword or destroying Minka town. All I can tell you
+                    is about Haruto Kaiba the man I knew.
+                    <br />
+                    <br />
+                    I met him way back when he was journeying through the
+                    country side. His small frame painted a wrong picture of his
+                    abilities and his sword was even less impressive. It looked
+                    old brittle and rusted. At first I laughed at the sight but
+                    as time passed I came to fear the weapon. Haruto never
+                    wanted to tell me where he got it from, even though I asked
+                    him every time I saw him. The double sided blade with its
+                    rusted look and the skull on the end of the hilt were
+                    symbols that he came to be know by. I know many called him
+                    Chi no okui (blood glutton), but only few knew that, that
+                    was the name of his trusty sword. “I always wondered who
+                    started calling Haruto that, he was one of the kindest men I
+                    knew. He often helped villager is his passing by.
+                    <br />
+                    During the time of turmoil back in the days ‘Sakura wars’.
+                    This very village were you stand today was saved by him.
+                    Many soldiers from the Hanoko clan came to our village
+                    demanding food men and women. Many tried to resist but was
+                    met with pure evil. Many men that tried to make a stand were
+                    cut down in form of their loved ones, even our elders were
+                    killed. I tried to protect as many as I could but these
+                    hands were not made to fight, “as my father told me”. I
+                    gathered as much people I could and hid them in the woods.
+                    The gods must have been with me that day. As I was captured
+                    by a Hanoko scum, I heard more screams from a distance. It
+                    was him he was walking through as if nothing was wrong. The
+                    soldiers approached him with pure evil intent. Haruto swung
+                    his blade with the speed of lightning. That is the first
+                    time I saw the evil of that sword.
+                    <br />
+                    As the first slash connected with the attacking soldier.
+                    That cursed sword seemed to change. The blood that was left
+                    from its victim on its rusted blade. This is when I saw it,
+                    the sword started changing. I swear I saw the blood being
+                    absorbed. Two soldiers rushed Haruto from behind he moved
+                    out the way, his speed that of a shadow. As he slashed them
+                    bot with one confident swing. As the slashes continued the
+                    rusted sword started transforming, the rust slowly started
+                    dissolving the more the blade came in contact with the red
+                    nectar that left the soldiers bodies. Haruto made sure to
+                    finish of the soldiers he showed no mercy. It was as if the
+                    sword demanded it. Some bodies sucked dried and discoloured
+                    scared the others. The rusted blade was now gone. I swear,
+                    the more that fell to the sword, the faster the blade and
+                    its wielder moved. The eerily skull on the hilt had turned
+                    red as if it filled with the blood from its victims. The
+                    faster he moved the more Hanoko soldiers fell. A dark mist
+                    started to form around Haruto. At every swing Chi no oku
+                    claimed more blood from its victims. Or should I say drink?
+                    I saw droplets make their way to the sword if they strayed
+                    too far away. Haruto dragged the his blade over the blood
+                    drenched floor and al is could see was the blood being
+                    sucked up from the floor. A true glutton, I could not
+                    believe my eyes. One of the remaining soldiers tried to run
+                    away. But Haruto would not give him that, he launched his
+                    trusted blade as an arrow at the fleeing soldier. The last I
+                    heard was an eerily scream as the sword pierced the soldiers
+                    skull. Haruto was the last man standing that day, as an evil
+                    smile took over his face. I would not have recognized him if
+                    it wasn’t for his clothing.
+                    <br />
+                    That was the first time I was afraid of my friend. There
+                    were at least 100 Hanoko soldiers in our village that day,
+                    none stood at the end. He was made a hero of our village
+                    that day.
+                    <br />
+                    “After telling this story I can understand why some call him
+                    a monster. But to me he was a friend and a hero.”
+                  </p>
+                </div>
+                <div className="col-md-6 col-sm-12 p-md-0">
+                  {" "}
+                  <img src="/assets/images/Derkizzle.jpeg" alt="Kiroh" />
+                </div>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
