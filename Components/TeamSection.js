2aa4f77d1cd94efb7965700da045484db4a3834f
Derkizzle <95419636+Derkizzle@users.noreply.github.com>
2021-12-04 00:21:32 +0000

    Update TeamSection.js

--- Components/TeamSection.js
+++ Components/TeamSection.js
@@ -18,7 +18,7 @@ const TeamSection = () => {
               <a
                 href="#"
                 className="assetbender w-inline-block"
-                style={{ backgroundImage: `url('/assets/images/Derkpunk4293.png')` }}
+                style={{ backgroundImage: `url('/assets/images/Derkpunk4293trans.png')` }}
               >
                 <div className="corner-white-copy bottom-right small" />
                 <div className="corner-white-copy bottom-right small _32" />
