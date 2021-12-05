86eb6efd3de3d8489939eb16a898d6afb129fec2
Derkizzle <95419636+Derkizzle@users.noreply.github.com>
2021-12-05 18:19:32 +0000

    Update whitepaper.js

--- Components/Others/whitepaper.js
+++ Components/Others/whitepaper.js
@@ -315,6 +315,16 @@ const whitepaper = () => {
                     The total cost to mint all of the Warriors and Samsar will
                     be 840,000,000 $HONR.
                   </p>
+                  <p className="text-white-opacity-2 mb-40 max-width dilema font-16">
+                    Whilst 1 $HONR will always = 1 $HONR, we will provide
+                    liquidity on Uniswap to allow more users to enter the Shadow
+                    world.
+                    <br />
+                    <br />
+                    The total cost to mint all of the Warriors and Samsar will
+                    be 840,000,000 $HONR.
+                  </p>
+  
                   <h3>Samsar</h3>
 
                   <p className="text-white-opacity-2 mb-40 max-width dilema font-16">
