cba4187da8f3809e93963ebabfec3ef77cee6297
Derkizzle <95419636+Derkizzle@users.noreply.github.com>
2021-12-05 20:19:52 +0000

    Update Footer.js

--- Components/Footer.js
+++ Components/Footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
     <div className="section footer wf-section">
       <div className="container">
         <Row>
-          <Col md="3">
-            <p className="text-extra-small align-left text-white-opacity mb-10">
+          <Col md="2">
+            <p className="text-extra-small align-left text-white-opacity mb-5">
               Shadow Quest. All Rights Reserved.{" "}
             </p>
           </Col>
