70f239734121cefefd666f53e50e0253edb1117c
Derkizzle <95419636+Derkizzle@users.noreply.github.com>
2021-12-05 18:35:17 +0000

    Update whitepaper.js

--- Components/Others/whitepaper.js
+++ Components/Others/whitepaper.js
@@ -306,7 +306,10 @@ const whitepaper = () => {
                       </tr>
                     </tbody>
                   </table>
-
+  
+                  <p className="text-white-opacity-2 mb-40 max-width dilema font-16">
+                  </p>
+  
                   <p className="text-white-opacity-2 mb-40 max-width dilema font-16">
                     Whilst 1 $HONR will always = 1 $HONR, we will provide
                     liquidity on Uniswap to allow more users to enter the Shadow
